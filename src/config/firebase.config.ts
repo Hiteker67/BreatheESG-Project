d92c74d6d673800da3fdfbb34dfa107ae9bbbbfd
hiteker67 <vkc.stw@gmail.com>
2024-06-05 17:49:29 +0530

    Submited full stack project on github.

---
+++ src/config/firebase.config.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDZl_EOAHz7-4TVDsF1q6n4NPnRLY_pFEE",
+  authDomain: "breathe-esg-be9b2.firebaseapp.com",
+  databaseURL: "https://breathe-esg-be9b2-default-rtdb.firebaseio.com",
+  projectId: "breathe-esg-be9b2",
+  storageBucket: "breathe-esg-be9b2.appspot.com",
+  messagingSenderId: "1003169489798",
+  appId: "1:1003169489798:web:ed3fa6bdeb728fb61e0207"
+};
